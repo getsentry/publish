d1c91d110b251d0a27bb2288faa60c6cc511864e
Burak Yigit Kaya <byk@sentry.io>
2021-04-14 17:34:18 +0300

    feat: Save publish state on failed publish runs (#231)
    Now with tests!

--- src/publish/__tests__/fail.js
+++ src/publish/__tests__/fail.js
@@ -1,3 +1,6 @@
+jest.mock("fs");
+
+const fs = require("fs");
 const fail = require("../fail.js").default;
 
 function deepFreeze(object) {
@@ -34,37 +37,118 @@ const failArgs = deepFreeze({
       }),
     },
     issues: {
+      get: jest.fn(),
+      update: jest.fn(),
       createComment: jest.fn(),
       removeLabel: jest.fn(),
     },
   },
 });
 
-const runOnFail = async () => fail(failArgs);
+beforeAll(() => {
+  process.env.GITHUB_WORKSPACE = ".";
+  fs.promises = {};
+  fs.promises.readFile = jest.fn(async () =>
+    JSON.stringify({published: {lol: true, hey: false, github: true}}),
+  );
 
-test("create comment", async () => {
-  await runOnFail();
+  failArgs.github.issues.get.mockReturnValue({
+    data: {
+      body: `
+Requested by: @BYK
 
-  const createComment = failArgs.github.issues.createComment;
-  expect(createComment).toHaveBeenCalledTimes(1);
-  expect(createComment).toHaveBeenCalledWith({
-    owner: "getsentry",
-    repo: "publish",
-    issue_number: "211",
-    body:
-      "Failed to publish: [run#1234](https://github.com/getsentry/sentry/actions/runs/1234/logs)\n\n_Bad branch? You can [delete with ease](https://github.com/getsentry/sentry/branches/all?query=21.3.1) and start over._",
+Quick links:
+- [View changes](https://github.com/getsentry/sentry/compare/21.3.0...refs/heads/releases/21.3.1)
+- [View check runs](https://github.com/getsentry/sentry/commit/7e5ca7ed5581552de066e2a8bc295b8306be38ac/checks/)
+
+Assign the **accepted** label to this issue to approve the release.
+
+### Targets
+- [ ] github
+- [ ] pypi
+- [ ] docker[release]
+- [ ] docker[latest]  
+`,
+    },
   });
 });
 
-test("remove label", async () => {
-  await runOnFail();
-
-  const removeLabel = failArgs.github.issues.removeLabel;
-  expect(removeLabel).toHaveBeenCalledTimes(1);
-  expect(removeLabel).toHaveBeenCalledWith({
-    owner: "getsentry",
-    repo: "publish",
-    issue_number: "211",
-    name: "accepted",
+describe.each([false, true])("state file exists: %s", (stateFileExists) => {
+  beforeEach(async () => {
+    fs.existsSync = jest.fn(() => stateFileExists);
+    await fail(failArgs);
+    expect(fs.existsSync).toHaveBeenCalledTimes(1);
+    // This is process.env.GITHUB_WORKSPACE + / filename
+    expect(fs.existsSync).toHaveBeenCalledWith("./.craft-publish-21.3.1.json");
   });
+
+  test("create comment", async () => {
+    const createComment = failArgs.github.issues.createComment;
+    expect(createComment).toHaveBeenCalledTimes(1);
+    expect(createComment).toHaveBeenCalledWith({
+      owner: "getsentry",
+      repo: "publish",
+      issue_number: "211",
+      body:
+        "Failed to publish: [run#1234](https://github.com/getsentry/sentry/actions/runs/1234/logs)\n\n_Bad branch? You can [delete with ease](https://github.com/getsentry/sentry/branches/all?query=21.3.1) and start over._",
+    });
+  });
+
+  test("remove label", async () => {
+    const removeLabel = failArgs.github.issues.removeLabel;
+    expect(removeLabel).toHaveBeenCalledTimes(1);
+    expect(removeLabel).toHaveBeenCalledWith({
+      owner: "getsentry",
+      repo: "publish",
+      issue_number: "211",
+      name: "accepted",
+    });
+  });
+
+  if (stateFileExists) {
+    test("restore publish state", async () => {
+      expect(failArgs.github.issues.get).toHaveBeenCalledTimes(1);
+      expect(failArgs.github.issues.get.mock.calls[0]).toMatchInlineSnapshot(`
+        Array [
+          Object {
+            "issue_number": "211",
+            "owner": "getsentry",
+            "repo": "publish",
+          },
+        ]
+      `);
+
+      expect(failArgs.github.issues.update).toHaveBeenCalledTimes(1);
+      expect(failArgs.github.issues.update.mock.calls[0])
+        .toMatchInlineSnapshot(`
+        Array [
+          Object {
+            "body": "
+        Requested by: @BYK
+
+        Quick links:
+        - [View changes](https://github.com/getsentry/sentry/compare/21.3.0...refs/heads/releases/21.3.1)
+        - [View check runs](https://github.com/getsentry/sentry/commit/7e5ca7ed5581552de066e2a8bc295b8306be38ac/checks/)
+
+        Assign the **accepted** label to this issue to approve the release.
+
+        - [x] github
+        - [ ] pypi
+        - [ ] docker[release]
+        - [ ] docker[latest]
+        - [x] lol
+        - [ ] hey
+        ",
+            "issue_number": "211",
+            "owner": "getsentry",
+            "repo": "publish",
+          },
+        ]
+      `);
+    });
+  } else {
+    test("don't modify issue body", () => {
+      expect(failArgs.github.issues.update).not.toHaveBeenCalled();
+    });
+  }
 });
--- src/publish/fail.js
+++ src/publish/fail.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 exports.default = async function fail({context, github, inputs}) {
   const {repo, version} = inputs;
 
@@ -9,7 +11,66 @@ exports.default = async function fail({context, github, inputs}) {
     })
   ).data;
   const issue_number = context.payload.issue.number;
+
+  const CRAFT_STATE_FILE_PATH = `${process.env.GITHUB_WORKSPACE}/.craft-publish-${version}.json`;
+  let updateIssueBodyRequest;
+  if (fs.existsSync(CRAFT_STATE_FILE_PATH)) {
+    const issueRequest = github.issues.get({
+      ...repoInfo,
+      issue_number,
+    });
+
+    const craftStateRequest = fs.promises
+      .readFile(CRAFT_STATE_FILE_PATH, {encoding: "utf-8"})
+      .then((data) => JSON.parse(data));
+
+    const [{data: issue}, craftState] = await Promise.all([
+      issueRequest,
+      craftStateRequest,
+    ]);
+
+    const targetsParser = /^### Targets$\s(^(?: *- \[[ x]\] [\w.\[\]-]+[ ]*$\n?)+)/m;
+    const declaredTargets = new Set();
+    let leadingSpaces = " ";
+    const newIssueBody = issue.body.replace(
+      targetsParser,
+      (_match, targetsSection) => {
+        let targetsText = targetsSection.trimRight();
+        const targetMatcher = /^( *)- \[[ x]\] ([\w.\[\]-]+)$/gim;
+        targetsText = targetsText.replace(
+          targetMatcher,
+          (_match, spaces, target) => {
+            leadingSpaces = spaces;
+            declaredTargets.add(target);
+            const x = craftState.published[target] ? "x" : " ";
+            return `${spaces}- [${x}] ${target}`;
+          },
+        );
+        const unlistedTargets = Object.keys(craftState.published)
+          .filter((target) => !declaredTargets.has(target))
+          .map(
+            (target) =>
+              `${leadingSpaces}- [${
+                craftState.published[target] ? "x" : " "
+              }] ${target}`,
+          )
+          .join("\n");
+        targetsText += `\n${unlistedTargets}\n`;
+        return targetsText;
+      },
+    );
+
+    updateIssueBodyRequest = github.issues.update({
+      ...repoInfo,
+      issue_number,
+      body: newIssueBody,
+    });
+  } else {
+    updateIssueBodyRequest = Promise.resolve();
+  }
+
   await Promise.all([
+    updateIssueBodyRequest,
     github.issues.createComment({
       ...repoInfo,
       issue_number,

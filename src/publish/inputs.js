88b505ce20ee986a59756b06df50aa51da8cffb0
Kamil Ogórek <kamil.ogorek@gmail.com>
2022-02-25 16:00:05 +0100

    feat: Allow for passing custom merge target (#838)

--- src/publish/inputs.js
+++ src/publish/inputs.js
@@ -6,6 +6,16 @@ exports.default = async function inputs({ context }) {
     : "";
   const path = "." + (titleMatch.path || "");
 
+  // https://docs.github.com/en/get-started/using-git/dealing-with-special-characters-in-branch-and-tag-names#naming-branches-and-tags
+  const mergeTargetParser = /^Merge target: (?<merge_target>[\w.\-/]+)$/m;
+  const mergeTargetMatch = context.payload.issue.body.match(
+    mergeTargetParser
+  )
+  let merge_target = '';
+  if (mergeTargetMatch && mergeTargetMatch.groups) {
+    merge_target = mergeTargetMatch.groups.merge_target || ''
+  }
+
   const targetsParser = /^(?!### Targets$\s)(?: *- \[[ x]\] [\w.[\]-]+$(?:\r?\n)?)+/m;
   const targetsMatch = context.payload.issue.body.match(targetsParser);
   let targets;
@@ -16,5 +26,5 @@ exports.default = async function inputs({ context }) {
     );
   }
 
-  return { ...titleMatch, path, dry_run, targets };
+  return { ...titleMatch, merge_target, path, dry_run, targets };
 };

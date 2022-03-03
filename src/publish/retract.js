1e7956f75bb4bb081bc489a77dceacde64207c9f
Kamil Ogórek <kamil.ogorek@gmail.com>
2022-03-03 14:46:36 +0100

    Add release branch link and fix retract script path

--- src/publish/retract.js
+++ src/publish/retract.js
@@ -1,11 +1,16 @@
-exports.default = async function success({ context, github }) {
+exports.default = async function success({ context, github, inputs }) {
   const repoInfo = context.repo;
+  const { repo, version } = inputs;
 
   await Promise.all([
     github.rest.issues.createComment({
       ...repoInfo,
       issue_number: context.payload.issue.number,
-      body: `Release request retracted by @${context.payload.sender.login}`,
+      body: `Release request retracted by @${
+        context.payload.sender.login
+      }.\nYou may also want to remove your [release branch](https://github.com/getsentry/${repo}/branches/all?query=${encodeURIComponent(
+        version
+      )}.`,
     }),
 
     github.rest.issues.update({

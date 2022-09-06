68d11d983915475d836515feec1fbb1307afdc61
Matt Gaunt-Seo @ Sentry.io <112419115+mattgauntseo-sentry@users.noreply.github.com>
2022-09-06 13:59:52 -0700

    Switch start.js to a node script (#1343)

--- src/modules/post-workflow-details.js
+++ src/modules/post-workflow-details.js
@@ -1,17 +1,19 @@
-exports.default = async function start({ context, github }) {
+async function postWorkflowDetails({context, octokit}) {
   const { repo: publishRepo, runId: run_id } = context;
   const { number: issue_number } = context.payload.issue;
 
   const workflowInfo = (
-    await github.rest.actions.getWorkflowRun({
+    await octokit.rest.actions.getWorkflowRun({
       ...publishRepo,
       run_id,
     })
   ).data;
 
-  return github.rest.issues.createComment({
+  return octokit.rest.issues.createComment({
     ...publishRepo,
     issue_number,
     body: `Publishing: [run#${run_id}](${workflowInfo.html_url})`,
   });
-};
+}
+
+module.exports = postWorkflowDetails;

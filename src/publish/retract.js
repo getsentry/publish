0e7252edb18134130152521241b1a24eaedc2491
Burak Yigit Kaya <byk@sentry.io>
2026-02-10 22:06:34 +0000

    ref: Remove #retract command
    Engineers now have access to close publish issues directly, making the
    #retract comment command redundant. This removes the workflow, module,
    entry point, tests, and documentation for the feature.

--- src/publish/retract.js
+++
@@ -1,9 +0,0 @@
-const github = require('@actions/github');
-const retract = require('../modules/retract-release');
-const {getGitHubToken} = require('../libs/github');
-
-const context = github.context;
-const octokit = github.getOctokit(getGitHubToken());
-const inputs = JSON.parse(process.env.PUBLISH_ARGS);
-
-retract({context, octokit, inputs});
